80e398e89524027bb4297e0561a24d82175d6d75
Daniel <d@stuhrs.dk>
2025-04-17 10:52:27 +0200

    Delete file. Add insecure str copy. Add .gitignore

---
+++ insecure_string_copy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFSIZE 128
+
+// Insecure version
+// int main(int argc, char *argv[])
+// {
+//     char buf[BUFSIZE];
+
+//     strcpy(buf, argv[1]);
+//     printf("%s \n", buf);
+
+//     return 0;
+// }
+
+int main(int argc, char *argv[])
+{
+    char buf[BUFSIZE];
+
+    strncpy(buf, argv[1], BUFSIZE - 1); // Use strncpy to prevent buffer overflow
+
+    printf("String: %s\n", buf);
+    printf("String length: %d\n", strlen(buf));
+
+    return 0;
+}
