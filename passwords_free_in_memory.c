20d85e70c0a41b143f0538984c474028bf079988
Daniel <d@stuhrs.dk>
2025-04-17 13:24:50 +0200

    Add passwords_free_in_memory.c

---
+++ passwords_free_in_memory.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PASSWORD_SIZE 256
+
+
+// int main(int argc, char *argv[])
+// {
+//     char *password = malloc(PASSWORD_SIZE);
+//     if (password == NULL) {
+//         fprintf(stderr, "Memory allocation failed\n");
+//         return 1;
+//     }
+
+//     strncpy(password, argv[1], PASSWORD_SIZE - 1);
+
+//     printf("Password address: %p\n", &password);
+//     printf("Entered password: %.*s \n", sizeof(password), password);
+
+//     // Check password...
+
+//     free(password);
+// }
+
+
+int main(int argc, char *argv[])
+{
+    char *password = malloc(PASSWORD_SIZE);
+    if (password == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
+    strncpy(password, argv[1], PASSWORD_SIZE - 1);
+
+    void *ptr = (void *) password;
+    printf("Password address: %p\n", &password);
+    printf("Entered password: %.*s \n", sizeof(password), password);
+
+    // Check password...
+
+    memset(password, 0, PASSWORD_SIZE); // Clear the password from memory
+    free(password);
+}
